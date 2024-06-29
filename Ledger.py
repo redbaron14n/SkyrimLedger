8ba8e4fc1bf9d6e3136cfd9128a5aa6a78f25034
Ian <redbaron14n@gmail.com>
2024-06-29 17:01:06 -0500

    Began 'main' function in 'Ledger.py' and created 'show_menu' function in 'MainMenu.py'.

--- Ledger.py
+++ Ledger.py
@@ -3,3 +3,19 @@
 # Skyrim Ledger
 # Driver file
 # ###################
+
+import MainMenu as mm
+
+def main():
+
+    """
+    Drives program.
+
+    @param na : na
+
+    @return na : na
+    """
+
+    mm.show_menu()
+
+main()
--- MainMenu.py
+++ MainMenu.py
@@ -3,3 +3,18 @@
 # Skyrim Ledger
 # Main menu subfile
 # ###################
+
+def show_menu():
+
+    """
+    Prints menu options for user.
+
+    @param na : na
+
+    @return na : na
+    """
+
+    print("Please pick an option:")
+    print("\t(L): Edit list")
+    print("\t(P): Edit prices")
+    print("\t(S): Edit skills")
